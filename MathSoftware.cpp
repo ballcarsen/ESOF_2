234ffdd850c397242d0c019f19f47b9baa63d02c
Mitchell Black <mitchellblack@Mitchells-MacBook-Pro.local>
2018-09-15 13:47:11 -0600

    added insertion

--- MathSoftware.cpp
+++ MathSoftware.cpp
@@ -100,7 +100,10 @@ public:
       for(int i=0; i<size; i++){
         int j=i;
         while(j>0 && arr[j]<arr[j-1]){
-          int temp=
+          int temp=arr[j-1];
+          arr[j-1]=arr[j];
+          arr[j]=temp;
+          j--;
         }
       }
     }

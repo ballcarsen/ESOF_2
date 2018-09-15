261c0ace474e863d487ee590fa31b6c7385e2da5
Carsen Ball <ballcarsen@gmail.com>
2018-09-15 13:25:25 -0600

    added some header files

---
+++ MTool.h
@@ -0,0 +1,8 @@
+//
+// Created by Carsen Ball on 9/15/18.
+//
+
+#ifndef ESOF_2_MTOOL_H
+#define ESOF_2_MTOOL_H
+
+#endif //ESOF_2_MTOOL_H
--- Mathematica.cpp
+++ Mathematica.cpp
@@ -2,8 +2,11 @@
 // Created by Carsen Ball on 9/15/18.
 //
 
+#include "MathSoftware.h"
+
 class Mathematica {
 public:
+    Mathematica();
 
 };
 
